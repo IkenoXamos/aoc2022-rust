9a7600f4b4171624ef0ba9de6b5dc80c0f1cc144
IkenoXamos <moberlies1337@gmail.com>
2023-02-15 02:18:17 -0500

    Day 1 solution using sorting ported from original project

---
+++ src/day1.rs
@@ -0,0 +1,103 @@
+use std::ops::Deref;
+
+use itertools::Itertools;
+
+// #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Elf(Vec<u32>);
+
+impl Deref for Elf {
+    type Target = Vec<u32>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl FromIterator<u32> for Elf {
+    fn from_iter<T: IntoIterator<Item = u32>>(iter: T) -> Self {
+        Self(Vec::from_iter(iter))
+    }
+}
+
+#[aoc_generator(day1, sorting)]
+pub fn parse_input_sorting(input: &str) -> Vec<Elf> {
+    input
+        .split("\n\n")
+        .map(|line| {
+            line.lines()
+                .map(|line| line.trim().parse::<u32>().unwrap())
+                .collect()
+        })
+        .collect()
+}
+
+#[aoc(day1, part1, sorting)]
+pub fn solve_part1_sorting(input: &Vec<Elf>) -> u32 {
+    input
+        .iter()
+        .map(|calories| calories.iter().sum::<u32>())
+        .sorted_by(|a, b| b.cmp(&a))
+        .nth(0)
+        .unwrap_or_default()
+}
+
+#[aoc(day1, part2, sorting)]
+pub fn solve_part2_sorting(input: &Vec<Elf>) -> u32 {
+    input
+        .iter()
+        .map(|calories| calories.iter().sum::<u32>())
+        .sorted_by(|a, b| b.cmp(&a))
+        .take(3)
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(
+            solve_part1_sorting(&parse_input_sorting(
+                "1000\n\
+                2000\n\
+                3000\n\
+                \n\
+                4000\n\
+                \n\
+                5000\n\
+                6000\n\
+                \n\
+                7000\n\
+                8000\n\
+                9000\n\
+                \n\
+                10000"
+            )),
+            24000
+        );
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(
+            solve_part2_sorting(&parse_input_sorting(
+                "1000\n\
+                2000\n\
+                3000\n\
+                \n\
+                4000\n\
+                \n\
+                5000\n\
+                6000\n\
+                \n\
+                7000\n\
+                8000\n\
+                9000\n\
+                \n\
+                10000"
+            )),
+            45000
+        );
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate aoc_runner;
 #[macro_use]
 extern crate aoc_runner_derive;
 
+pub mod day1;
 pub mod utils;
 
 aoc_lib! { year = 2022 }
